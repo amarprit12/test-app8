6fcfdbb5e33109df85e36100f0cc50aa63817022
Amarprit <amarpritkaur.kharbanda@citrix.com>
2024-02-16 13:27:42 +0530

    learning useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { React, useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState("Test1");
+
+  // called only when component is mounted
+  useEffect(() => {
+    console.log("component mounted");
+  }, []);
+
+  // called when count is incremented or count state is updated
+  useEffect(() => {
+    console.log("component mounted1");
+  }, [count]);
+
+  useEffect(() => {
+    console.log("component mounted2");
+  }, [data]);
+
+  function updateState() {
+    setCount(count + 1);
+  }
+  function updateData() {
+    setData("Test2");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Learning usestate {count} times</h1>
+      <h2>Data {data}</h2>
+      <button onClick={updateState}>Click Me</button>
+      <button onClick={updateData}>Click Me</button>
+    </>
   );
 }
 
